a87d09d40bf4a160a4c9fa57731233f1c0ee42b2
MiguelIbanezG <miguel.ibanez.gon@gmail.com>
2024-05-28 13:05:14 +0200

    k

--- Miguel-Ibáñez-Cards/app/src/main/java/es/uam/eps/dadm/cards/Home.kt
+++ Miguel-Ibáñez-Cards/app/src/main/java/es/uam/eps/dadm/cards/Home.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 
+//
 @Composable
 fun Home(navController: NavController, viewModel: CardViewModel) {
     Surface(
